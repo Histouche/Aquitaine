73aa0d2637d8386faf09e7e676d5023289748409
Jonathan <jnt.rodrigues68@gmail.com>
2018-01-25 23:18:07 +0100

    creation site header, site footer, list annonce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { SiteFooterComponent } from './site-footer/site-footer.component';
+import { ListAnnonceComponent } from './list-annonce/list-annonce.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent,
+    ListAnnonceComponent
   ],
   imports: [
     BrowserModule
