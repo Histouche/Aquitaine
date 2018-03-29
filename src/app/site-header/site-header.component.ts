624e387262de41a6797a72c4b525055d5be8beb3
Jonathan <jnt.rodrigues68@gmail.com>
2018-03-29 21:47:56 +0200

    création de services : constantes, link, variables et user

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, PLATFORM_ID, Inject, Injectable } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-site-header',
   templateUrl: './site-header.component.html',
-  styleUrls: ['./site-header.component.scss']
+  styleUrls: ['./site-header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SiteHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngOnInit() {
   }
